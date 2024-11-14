65adc4bbff0ef7e933ec5c7d15186fe5c0ce2f30
fL03 <91798342+fL0390@users.noreply.github.com>
2024-11-14 10:49:52 +0100

    Done.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const label = document.querySelector('.label');
-
+//Modifica el 'label' quan aquest esta seleccionat.
 inputField.addEventListener('focus', function () {
     label.style.fontSize = '12px';
     label.style.color = '#007bff';
